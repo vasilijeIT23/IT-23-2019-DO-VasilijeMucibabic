9b72f971c7fd3dfc5078fc46a80b3d732af98c87
vasilijeIT23 <mucibabic.vasilije21@gmail.com>
2026-03-17 21:31:33 +0100

    changed so each mode has their own status message, so the message isn't hardcoded in the controller

--- src/app/drawing/enums/Modes.java
+++ src/app/drawing/enums/Modes.java
@@ -1,12 +1,22 @@
 package app.drawing.enums;
 
 public enum Modes {
-    POINT,
-    LINE,
-    RECTANGLE,
-    CIRCLE,
-    DONUT,
-    MODIFY,
-    DELETE,
-    SELECT
+    POINT("Tool: POINT | Click to place a point."),
+    LINE("Tool: LINE | Click start, then click end."),
+    RECTANGLE("Tool: RECTANGLE | Click upper left point."),
+    CIRCLE("Tool: CIRCLE | Click the center."),
+    DONUT("Tool: DONUT | Click the center."),
+    MODIFY("Tool: MODIFY | Click a shape to modify."),
+    DELETE("Tool: DELETE | Click a shape to delete."),
+    SELECT("Tool: SELECT | Click a shape to select.");
+
+    private final String statusMessage;
+
+    Modes(String statusMessage) {
+        this.statusMessage = statusMessage;
+    }
+
+    public String getStatusMessage() {
+        return statusMessage;
+    }
 }
