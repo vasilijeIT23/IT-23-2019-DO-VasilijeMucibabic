596dc60887114f023b327d8ecb370db7312b3ecc
vasilijeIT23 <mucibabic.vasilije21@gmail.com>
2026-03-17 21:37:00 +0100

    had issue with modify. current state of shape wasn't loaded at all. now when creating modify forms I'm passing the shape that is being modified, and from it I'm extracting and loading current values

--- src/app/drawing/components/FieldSpecs.java
+++ src/app/drawing/components/FieldSpecs.java
@@ -2,26 +2,7 @@ package app.drawing.components;
 
 import app.drawing.enums.FieldTypes;
 
-import java.awt.*;
+public record FieldSpecs(String key, String label, FieldTypes type, Object defaultValue, Integer min, Integer max,
+                         boolean required) {
 
-public final class FieldSpecs {
-
-    public final String key;
-    public final String label;
-    public final FieldTypes type;
-    public final Object defaultValue;
-    public final Integer min;
-    public final Integer max;
-    public final boolean required;
-
-    public FieldSpecs(String key, String label, FieldTypes type,
-                      Object defaultValue, Integer min, Integer max, boolean required) {
-        this.key = key;
-        this.label = label;
-        this.type = type;
-        this.defaultValue = defaultValue;
-        this.min = min;
-        this.max = max;
-        this.required = required;
-    }
 }
--- src/app/drawing/components/FormLibrary.java
+++ src/app/drawing/components/FormLibrary.java
@@ -16,14 +16,16 @@ public final class FormLibrary {
             )
     );
 
-    public static final FormSpecs MODIFY_POINT_FIELDS = new FormSpecs(
-            "Point Properties",
-            List.of(
-                    new FieldSpecs("x", "X Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("y", "Y Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("border", "Border color", FieldTypes.COLOR, Color.BLACK, null, null, false)
-            )
-    );
+    public static FormSpecs modifyPointFields(app.geometry.Point p) {
+        return new FormSpecs(
+                "Point properties",
+                List.of(
+                        new FieldSpecs("x", "X Axis:", FieldTypes.INT, p.getX(), 1, 2000, true),
+                        new FieldSpecs("y", "Y Axis:", FieldTypes.INT, p.getY(), 1, 2000, true),
+                        new FieldSpecs("border", "Border color", FieldTypes.COLOR, p.getColor(), null, null, false)
+                )
+        );
+    }
 
     public static final FormSpecs CREATE_LINE_FIELDS = new FormSpecs(
             "Line Properties",
@@ -32,16 +34,18 @@ public final class FormLibrary {
             )
     );
 
-    public static final FormSpecs MODIFY_LINE_FIELDS = new FormSpecs(
-            "Line Properties",
-            List.of(
-                    new FieldSpecs("x", "X Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("y", "Y Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("x2", "X Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("y2", "Y Axis:", FieldTypes.INT, 400, 1, 2000, true),
-                    new FieldSpecs("border", "Border color", FieldTypes.COLOR, Color.BLACK, null, null, false)
-            )
-    );
+    public static FormSpecs modifyLineFields(app.geometry.Line l) {
+        return new FormSpecs(
+                "Line properties",
+                List.of(
+                        new FieldSpecs("x", "X Axis:", FieldTypes.INT, l.getA().getX(), 1, 2000, true),
+                        new FieldSpecs("y", "Y Axis:", FieldTypes.INT, l.getA().getY(), 1, 2000, true),
+                        new FieldSpecs("x2", "X Axis:", FieldTypes.INT, l.getB().getX(), 1, 2000, true),
+                        new FieldSpecs("y2", "Y Axis:", FieldTypes.INT, l.getB().getY(), 1, 2000, true),
+                        new FieldSpecs("border", "Border color", FieldTypes.COLOR, l.getColor(), null, null, false)
+                )
+        );
+    }
 
     public static final FormSpecs CREATE_RECTANGLE_FIELDS = new FormSpecs(
             "Rectangle properties",
@@ -53,17 +57,19 @@ public final class FormLibrary {
             )
     );
 
-    public static final FormSpecs MODIFY_RECTANGLE_FIELDS = new FormSpecs(
-            "Rectangle properties",
-            List.of(
-                    new FieldSpecs("x", "X Axis:", FieldTypes.INT, 500, 1, 2000, true),
-                    new FieldSpecs("y", "Y Axis:", FieldTypes.INT, 500, 1, 2000, true),
-                    new FieldSpecs("width", "Width:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("height", "Height:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("border", "Border color", FieldTypes.COLOR, Color.BLACK, null, null, false),
-                    new FieldSpecs("fill", "Inner color", FieldTypes.COLOR, Color.WHITE, null, null, false)
-            )
-    );
+    public static FormSpecs modifyRectangleFields(app.geometry.Rectangle r) {
+        return new FormSpecs(
+                "Rectangle properties",
+                List.of(
+                        new FieldSpecs("x",      "X Axis:",      FieldTypes.INT,   r.getUpperLeftPoint().getX(), 1, 2000, true),
+                        new FieldSpecs("y",      "Y Axis:",      FieldTypes.INT,   r.getUpperLeftPoint().getY(), 1, 2000, true),
+                        new FieldSpecs("width",  "Width:",       FieldTypes.INT,   r.getWidth(),                 1, 2000, true),
+                        new FieldSpecs("height", "Height:",      FieldTypes.INT,   r.getHeight(),                1, 2000, true),
+                        new FieldSpecs("border", "Border color", FieldTypes.COLOR, r.getColor(),                 null, null, false),
+                        new FieldSpecs("fill",   "Inner color",  FieldTypes.COLOR, r.getInnerColor(),            null, null, false)
+                )
+        );
+    }
 
     public static final FormSpecs CREATE_CIRCLE_FIELDS = new FormSpecs(
             "Circle properties",
@@ -74,16 +80,18 @@ public final class FormLibrary {
             )
     );
 
-    public static final FormSpecs MODIFY_CIRCLE_FIELDS = new FormSpecs(
-            "Circle properties",
-            List.of(
-                    new FieldSpecs("x", "X Axis:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("y", "Y Axis:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("radius", "Radius:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("border", "Border color", FieldTypes.COLOR, Color.BLACK, null, null, false),
-                    new FieldSpecs("fill", "Inner color", FieldTypes.COLOR, Color.WHITE, null, null, false)
-            )
-    );
+    public static FormSpecs modifyCircleFields(app.geometry.Circle c) {
+        return new FormSpecs(
+                "Rectangle properties",
+                List.of(
+                        new FieldSpecs("x", "X Axis:", FieldTypes.INT, c.getCenter().getX(), 1, 2000, true),
+                        new FieldSpecs("y", "Y Axis:", FieldTypes.INT, c.getCenter().getY(), 1, 2000, true),
+                        new FieldSpecs("radius", "Radius:", FieldTypes.INT, c.getRadius(), 1, 2000, true),
+                        new FieldSpecs("border", "Border color", FieldTypes.COLOR, c.getColor(), null, null, false),
+                        new FieldSpecs("fill", "Inner color", FieldTypes.COLOR, c.getInnerColor(), null, null, false)
+                )
+        );
+    }
 
     public static final FormSpecs CREATE_DONUT_FIELDS = new FormSpecs(
             "Donut properties",
@@ -100,20 +108,22 @@ public final class FormLibrary {
             }
     );
 
-    public static final FormSpecs MODIFY_DONUT_FIELDS = new FormSpecs(
-            "Donut properties",
-            List.of(
-                    new FieldSpecs("x", "X Axis:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("y", "Y Axis:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("outer", "Outer radius:", FieldTypes.INT, 50, 1, 2000, true),
-                    new FieldSpecs("inner", "Inner radius:", FieldTypes.INT, 20, 1, 2000, true),
-                    new FieldSpecs("border", "Border color", FieldTypes.COLOR, Color.BLACK, null, null, false),
-                    new FieldSpecs("fill", "Inner color", FieldTypes.COLOR, Color.WHITE, null, null, false)
-            ),
-            values -> {
-                int outer = (Integer) values.get("outer");
-                int inner = (Integer) values.get("inner");
-                return inner < outer ? null : "Inner radius must be smaller than outer radius.";
-            }
-    );
+    public static FormSpecs modifyDonutFields(app.geometry.Donut d) {
+        return new FormSpecs(
+                "Rectangle properties",
+                List.of(
+                        new FieldSpecs("x", "X Axis:", FieldTypes.INT, d.getCenter().getX(), 1, 2000, true),
+                        new FieldSpecs("y", "Y Axis:", FieldTypes.INT, d.getCenter().getY(), 1, 2000, true),
+                        new FieldSpecs("outer", "Outer radius:", FieldTypes.INT, d.getRadius(), 1, 2000, true),
+                        new FieldSpecs("inner", "Inner radius:", FieldTypes.INT, d.getInnerRadius(), 1, 2000, true),
+                        new FieldSpecs("border", "Border color", FieldTypes.COLOR, d.getColor(), null, null, false),
+                        new FieldSpecs("fill", "Inner color", FieldTypes.COLOR, d.getInnerColor(), null, null, false)
+                ),
+                values -> {
+                    int outer = (Integer) values.get("outer");
+                    int inner = (Integer) values.get("inner");
+                    return inner < outer ? null : "Inner radius must be smaller than outer radius.";
+                }
+        );
+    }
 }
